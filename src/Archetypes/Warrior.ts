ef37dc49b7aeb684fa7ca006314506dd35129f07
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-09 15:53:24 -0300

    feat: criando a subclasse Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  static countWarriorInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior.countWarriorInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Warrior.countWarriorInstances;
+  }
+}
+
+export default Warrior;
