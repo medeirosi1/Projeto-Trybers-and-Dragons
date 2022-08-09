37de3e0e02af985643d642f2f53dd5462cfe7484
Thiago Medeiros <medeiroshs@hotmail.com>
2022-08-09 15:53:12 -0300

    feat: criando a subclasse Ranger

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  static countRangerInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger.countRangerInstances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Ranger.countRangerInstances;
+  }
+}
+
+export default Ranger;
